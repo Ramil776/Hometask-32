c8cfa119e233f79095da21f45c5668b01490014b
Ramil776 <ramil.sadigov00@mail.ru>
2024-06-26 15:05:27 +0400

    Hometask

---
+++ app.js
@@ -0,0 +1,25 @@
+// hometask
+let age = prompt("Nece yasiniz var?")
+if (age < 18) {
+    bildiris = "Icaze verilmedi"
+} else {
+    bildiris = "Icaze verildi"
+}
+console.log(bildiris);
+
+// hometask
+let year =prompt("Il daxil edin")
+if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+    console.log(year + "uzun ildir");
+} else {
+    console.log(year + "uzun il deil");
+}
+
+// hometask
+let numbers = prompt("Vergul qoyaraq 3 eded daxil edin")
+let number = numbers.split(" ")
+let num1 = [0]
+let num2 = [1]
+let num3 = [2]
+if (num1 > num20 || num2 > num3);
+{console.log();}
